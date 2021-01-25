761fa150513c44032ac0a7a91005b523d137d4c1
Facundo Gomez <gomezvera.f@gmail.com>
2021-01-25 16:35:07 +0100

    create initial home outline

---
+++ client/src/components/Nav.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export default class Nav extends Component {
+  render() {
+    return (
+      <div>
+      <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Education</a></li>
+        <li><a href="#">Experience</a></li>
+      </ul>
+      </div>
+    )
+  }
+}
