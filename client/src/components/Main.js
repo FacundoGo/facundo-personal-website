0505cd8e9d5d398edbb206f7aa0542091fff4f3c
Facundo Gomez <gomezvera.f@gmail.com>
2021-01-26 16:16:28 +0100

    Name the cards of the main page and add hover color

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -3,40 +3,19 @@ import React, { Component } from 'react'
 import Posts from './Posts'
 import CardDeck from 'react-bootstrap/CardDeck'
 import Card from 'react-bootstrap/Card'
+import './Main.css'
 
 export default class Main extends Component {
   render() {
     return (
-                  <CardDeck>
-                  <Posts></Posts>
-  <Card>
-    <Card.Img variant="top" src="holder.js/100px160" />
-    <Card.Body>
-      <Card.Title>Card title</Card.Title>
-      <Card.Text>
-        This card has supporting text below as a natural lead-in to additional
-        content.{' '}
-      </Card.Text>
-    </Card.Body>
-    <Card.Footer>
-      <small className="text-muted">Last updated 3 mins ago</small>
-    </Card.Footer>
-  </Card>
-  <Card>
-    <Card.Img variant="top" src="holder.js/100px160" />
-    <Card.Body>
-      <Card.Title>Card title</Card.Title>
-      <Card.Text>
-        This is a wider card with supporting text below as a natural lead-in to
-        additional content. This card has even longer content than the first to
-        show that equal height action.
-      </Card.Text>
-    </Card.Body>
-    <Card.Footer>
-      <small className="text-muted">Last updated 3 mins ago</small>
-    </Card.Footer>
-  </Card>
-</CardDeck>
+            <div id="container-main">
+                  <Posts name="Experience"></Posts>
+                  <Posts name="Education"></Posts>
+                  <Posts name="Software Projects"></Posts>
+                  <Posts name="Services"></Posts>
+                  <Posts name="Contact"></Posts>
+                  <Posts name="Surprise"></Posts>
+            </div>
     )
   }
 }
--- client/src/components/Posts.js
+++ client/src/components/Posts.js
@@ -8,14 +8,10 @@ export default class Posts extends Component {
 <Card>
     <Card.Img variant="top" src="holder.js/100px160" />
     <Card.Body>
-      <Card.Title>Card title</Card.Title>
-      <Card.Text>
-        This is a wider card with supporting text below as a natural lead-in to
-        additional content. This content is a little bit longer.
-      </Card.Text>
+      <Card.Title>{this.props.name}</Card.Title>
     </Card.Body>
     <Card.Footer>
-      <small className="text-muted">Last updated 3 mins ago</small>
+      <small className="text-muted">Go here!</small>
     </Card.Footer>
   </Card>
     )
