d193f51c49101a9b0a325d61297840ddc0d157fa
Facundo Gomez <gomezvera.f@gmail.com>
2021-02-08 18:15:59 +0100

    make the images clickable

--- client/src/components/Posts.js
+++ client/src/components/Posts.js
@@ -7,7 +7,7 @@ export default class Posts extends Component {
   render() {
     return (
 <Card style={{ width: '13rem' }}>
-    <Card.Img variant="top" src={this.props.img} />
+<Link to={`/${this.props.name}`}><Card.Img variant="top" src={this.props.img}/></Link>
     <Card.Footer>
     <Card.Title><Link to={`/${this.props.name}`}>{this.props.name}</Link></Card.Title>
     </Card.Footer>
