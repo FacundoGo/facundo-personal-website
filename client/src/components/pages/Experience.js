8802a9b0aef3b26cad5bb412217eaf1fe310913c
Facundo Gomez <gomezvera.f@gmail.com>
2021-02-08 18:13:20 +0100

    finalize the formating of the experience section

--- client/src/components/pages/Experience.js
+++ client/src/components/pages/Experience.js
@@ -1,78 +1,52 @@
 import React, { Component } from 'react'
 import Navbar from '../Navbar';
-import Card from 'react-bootstrap/Card'
-import Accordion from 'react-bootstrap/Accordion'
-import Button from 'react-bootstrap/Button'
+import './Experience.css';
 
 export default class Experience extends Component {
   render() {
     return (
-      <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+      <div className="main">
         <Navbar></Navbar>
-        <h1 style={{marginTop: '3%'}}>Experience</h1>
-        <Accordion style={{width: '50vw', marginTop: '3%'}}defaultActiveKey="0">
-  <Card>
-    <Card.Header>
-      <Accordion.Toggle as={Button} variant="link" eventKey="0">
-        <h4>Chief Operating Officer</h4>
-        <h4>Leonyte Biosystems - Berlin, Germany</h4>
-      </Accordion.Toggle>
-    </Card.Header>
-    <Accordion.Collapse eventKey="0">
-      <Card.Body><ul style={{textAlign: 'left', width: '40vw'}}>
+        <h1 className="header">Experience</h1>
+        <div className="entries">
+        <div className="entry">
+        <h3><a href="www.leonyte.com" target="_blank" className="companyLink">Leonyte Biosystems - </a><span className="role">Chief Operating Officer</span></h3>
+        <h5 className="dates">Jan/2017 - Nov/2020</h5>
+        <h5 className="location">Berlin, Germany</h5>
+        <ul>
         <li>Fundraised over €3.4 million in 3 rounds</li>
         <li>Defined and implemented scalable processes for a high performing technical team of over 17 scientists and engineers</li>
         <li>Developed and managed entire B2B accounts in the US and Europe</li>
       </ul>
-      </Card.Body>
-    </Accordion.Collapse>
-  </Card>
-  <Card>
-    <Card.Header>
-      <Accordion.Toggle as={Button} variant="link" eventKey="1">
-        <h4>Entrepreneur-in-Residence</h4>
-        <h4>MCB Labs - Berlin, Germany</h4>
-      </Accordion.Toggle>
-    </Card.Header>
-    <Accordion.Collapse eventKey="1">
-      <Card.Body><ul style={{textAlign: 'left', width: '40vw'}}>
+        </div>
+        <div className="entry">
+        <h3><a href="www.mcblabs.com" target="_blank" className="companyLink">MCB Labs - </a><span className="role">Entrepreneur-in-Residence</span></h3>
+        <h5 className="dates">Jul/2016 - Dec/2016</h5>
+        <h5 className="location">Berlin, Germany</h5>
+        <ul>
         <li>Defined and built the initial customer pipeline for novel AI based sales training technology</li>
         <li>Created business case for novel pathogen-detection technology, including commercialisation and licensing over 13 patents</li>
       </ul>
-      </Card.Body>
-    </Accordion.Collapse>
-  </Card>
-  <Card>
-    <Card.Header>
-      <Accordion.Toggle as={Button} variant="link" eventKey="2">
-        <h4>Trading and Sales Operations Analyst</h4>
-        <h4>JP Morgan Chase - Buenos Aires, Argentina</h4>
-      </Accordion.Toggle>
-    </Card.Header>
-    <Accordion.Collapse eventKey="2">
-      <Card.Body><ul style={{textAlign: 'left', width: '40vw'}}>
+      </div>
+      <div className="entry">
+      <h3><a href="www.jpmorgan.com" target="_blank" className="companyLink">JP Morgan Chase - </a><span className="role">Trading and Sales Operations Analyst</span></h3>
+        <h5 className="dates">May/2014 - Jul/2015</h5>
+        <h5 className="location">Buenos Aires, Argentina</h5>
+        <ul>
         <li>Defined and built the initial customer pipeline for novel AI based sales training technology</li>
         <li>Created business case for novel pathogen-detection technology, including commercialisation and licensing over 13 patents</li>
       </ul>
-      </Card.Body>
-    </Accordion.Collapse>
-  </Card>
-  <Card>
-    <Card.Header>
-      <Accordion.Toggle as={Button} variant="link" eventKey="3">
-        <h4>Strategy Consultant</h4>
-        <h4>Paradigma - Buenos Aires, Argentina</h4>
-      </Accordion.Toggle>
-    </Card.Header>
-    <Accordion.Collapse eventKey="3">
-      <Card.Body><ul style={{textAlign: 'left', width: '40vw'}}>
+      </div>
+      <div className="entry">
+      <h3><a href="www.paradigma.com" target="_blank" className="companyLink">Paradigma - </a><span className="role">Strategy Consultant</span></h3>
+        <h5 className="dates">Oct/2013 - May/2014</h5>
+        <h5 className="location">Buenos Aires, Argentina</h5>
+        <ul>
         <li>Quantified the potential improvements available in the commercial banking operations in Banco Industrial</li>
         <li>Analysed and redesigned the Procurement and Accounts Payable operations of Monsanto benchmarking against other clients, advised on over 25 points of improvement</li>
       </ul>
-      </Card.Body>
-    </Accordion.Collapse>
-  </Card>
-</Accordion>
+      </div>
+      </div>
       </div>
     )
   }
